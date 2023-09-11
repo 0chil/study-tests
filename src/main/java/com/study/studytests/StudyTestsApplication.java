0594ec3bffafb79b795ffa37ee29105093aa0333
0chil <0@chll.it>
2023-09-11 14:43:53 +0900

    Initial Commit

---
+++ src/main/java/com/study/studytests/StudyTestsApplication.java
@@ -0,0 +1,13 @@
+package com.study.studytests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyTestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/studytests/StudyTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.studytests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
